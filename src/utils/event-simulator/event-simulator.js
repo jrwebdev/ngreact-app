f48078f040d4f4812ffd11496e5bf73d55840e70
jrwebdev <web@jravenscroft.co.uk>
2016-04-14 17:05:37 +1200

    Add key events

--- src/utils/event-simulator/event-simulator.js
+++ src/utils/event-simulator/event-simulator.js
@@ -15,6 +15,8 @@ const reactEventMap = {
     keyup: 'keyUp'
 };
 
+const keyEventData = (keyCode, eventData) => extend(eventData, {keyCode, which: keyCode, charCode: keyCode});
+
 const simulate = (el, event, eventData = {}) => {
 
     if (!el) {
@@ -39,6 +41,10 @@ const simulate = (el, event, eventData = {}) => {
     event = reactEventMap[event] || event;
     Simulate[event](el, eventData);
 };
+
+simulate.keyDown = (el, keyCode, eventData) => simulate(el, 'keyDown', keyEventData(keyCode, eventData));
+simulate.keyPress = (el, keyCode, eventData) => simulate(el, 'keyPress', keyEventData(keyCode, eventData));
+simulate.keyUp = (el, keyCode, eventData) => simulate(el, 'keyDown', keyEventData(keyCode, eventData));
 
 const key = (el, keyCode) => {
     let eventData = {keyCode, which: keyCode, charCode: keyCode};
