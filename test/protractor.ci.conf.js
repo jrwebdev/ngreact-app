592ef7f95cb731d30aaf49b20b9519f1c9d54a81
jrwebdev <web@jravenscroft.co.uk>
2016-04-21 13:34:19 +1200

    Use multiple browsers

--- test/protractor.ci.conf.js
+++ test/protractor.ci.conf.js
@@ -3,11 +3,15 @@ exports.config = {
     sauceUser: process.env.SAUCE_USERNAME,
     sauceKey: process.env.SAUCE_ACCESS_KEY,
 
-    capabilities: {
+    multiCapabilities: [{
         'browserName': 'chrome',
         'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
         'build': process.env.TRAVIS_BUILD_NUMBER
-    },
+    },{
+        'browserName': 'firefox',
+        'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
+        'build': process.env.TRAVIS_BUILD_NUMBER
+    }],
 
     specs: [
         './e2e/**/*.spec.js'
